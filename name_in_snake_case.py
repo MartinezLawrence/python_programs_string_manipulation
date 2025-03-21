a06eb368ac60561efbb8cb7fd9fefc73c39b48bc
unknown <lawrencekulet@gmail.com>
2025-03-22 02:23:27 +0800

    pseudocode

---
+++ name_in_snake_case.py
@@ -0,0 +1,6 @@
+# name in snake case
+# this program will convert the user's inputted full name in snake case
+
+# ask for the user's full name in incorrect casing
+# convert to snake case 
+# print the snake case name
