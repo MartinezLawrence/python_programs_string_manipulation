ff6f1931246c87e2ff060b0ab5a3d696acd34234
unknown <lawrencekulet@gmail.com>
2025-03-22 01:52:43 +0800

    pseudocode

---
+++ print_in_proper_casing.py
@@ -0,0 +1,6 @@
+# print in proper casing
+# this program will correct improper casing of a string
+
+# ask the user for his/her full name
+# convert the first letter of each word to uppercase by using the title() method
+# print the corrected name
