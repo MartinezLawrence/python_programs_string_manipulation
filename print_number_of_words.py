3138bf96ab1558f5c394d69c72a0f19d0eb0b55d
unknown <lawrencekulet@gmail.com>
2025-03-22 02:02:50 +0800

    pseudocode

---
+++ print_number_of_words.py
@@ -0,0 +1,5 @@
+# print number of words
+# this program will ask the user to enter a statemtent and then it will count the number of words
+
+# ask the user to enter a statement
+# split the statement into words and count the number of words
