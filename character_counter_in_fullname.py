01fbafafdf1a24baa5b9ab8d79d37f4a43c751eb
unknown <lawrencekulet@gmail.com>
2025-03-22 02:10:35 +0800

    character counter in fullname finished

--- character_counter_in_fullname.py
+++ character_counter_in_fullname.py
@@ -2,5 +2,10 @@
 # this program will count how many characters the inoutted full name has
 
 # ask for the full name
+fullname = input("Enter your full name: ")
+
 # count the characters in the full name
+number_of_characters = len(fullname)
+
 # print the number of characters in the full name
+print(number_of_characters)
