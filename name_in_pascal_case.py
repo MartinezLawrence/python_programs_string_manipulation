0c4cb5c8af0239694a60fa8858912b8394149de7
unknown <lawrencekulet@gmail.com>
2025-03-22 02:17:33 +0800

    name in pascal case finished4

--- name_in_pascal_case.py
+++ name_in_pascal_case.py
@@ -2,5 +2,10 @@
 # this program will ask the user to enter full name in incorrect casing and print it in pascal case
 
 # ask the user to enter full name in incorrect casing
+fullname = input("Enter your full name in incorrect casing: ")
+
 # convert the to pascal case
+fullname = fullname.title().split() # convert the full name to title case and split it into a list
+
 # print the name in pascal case
+print(''.join(fullname)) # join the list of names into a string and print it
