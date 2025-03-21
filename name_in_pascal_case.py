431358641e9f8f334a9149f32e2f300dc91e29cc
unknown <lawrencekulet@gmail.com>
2025-03-22 02:13:44 +0800

    pseudocode

---
+++ name_in_pascal_case.py
@@ -0,0 +1,6 @@
+# name in pascal case
+# this program will ask the user to enter full name in incorrect casing and print it in pascal case
+
+# ask the user to enter full name in incorrect casing
+# convert the to pascal case
+# print the name in pascal case
