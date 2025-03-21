bb835e7d673e82b43becc91c481c0dea5d8a2631
unknown <lawrencekulet@gmail.com>
2025-03-22 02:04:58 +0800

    word coun in a statement finished

--- print_number_of_words.py
+++ print_number_of_words.py
@@ -2,4 +2,10 @@
 # this program will ask the user to enter a statemtent and then it will count the number of words
 
 # ask the user to enter a statement
+statement = input("Enter a statement: ")
+
 # split the statement into words and count the number of words
+number_of_words = len(statement.split())
+
+# print the number of words in the statement
+print(number_of_words) 
