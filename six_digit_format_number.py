a2e2145e303ff46990eeb3f9c86e305431dc3b90
unknown <lawrencekulet@gmail.com>
2025-03-22 01:45:43 +0800

    six digit format finish

--- six_digit_format_number.py
+++ six_digit_format_number.py
@@ -2,4 +2,7 @@
 # this program will print an inputted number from 1 - 1000 and print it in a six digit format with leading zeros beginning to complete the six digit format
 
 # prompt the user to input a number from 1 - 1000
+number = int(input("Enter a number from 1 - 1000: "))
+
 # print the number in a six digit format with leading zeros
+print(f"{number:06}")   # print the number in a six digit format with leading zeros
