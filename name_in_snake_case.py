d072476a39b189b318f5d15f3318c0a55d0081d0
unknown <lawrencekulet@gmail.com>
2025-03-22 02:27:42 +0800

    name in snake case finished

--- name_in_snake_case.py
+++ name_in_snake_case.py
@@ -2,5 +2,10 @@
 # this program will convert the user's inputted full name in snake case
 
 # ask for the user's full name in incorrect casing
+fullname = input("Enter your full name in incorrect casing: ")
+
 # convert to snake case 
+snake_case = fullname.lower() # convert the full name to lowercase
+
 # print the snake case name
+print(snake_case.replace(' ', '_')) # replace the spaces with underscores and print the name in snake case
