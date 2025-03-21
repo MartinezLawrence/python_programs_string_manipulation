4673c4423c4d98bdd0f18059d083a922cadf5bd5
unknown <lawrencekulet@gmail.com>
2025-03-22 01:58:19 +0800

    pseudocode

---
+++ print_incorrect_casing.py
@@ -0,0 +1,5 @@
+# print incorrect casing
+# ask the user to input their fullname in incorrect casing and print each character of the input in reverse casing
+
+# prompt the user to input their fullname in incorrect casing
+# swap each character of the input to the opposite casing and print the result
