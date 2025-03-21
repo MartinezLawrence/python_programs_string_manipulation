ad2497559adc15a0e326f4893e5e88c0e5054396
unknown <lawrencekulet@gmail.com>
2025-03-22 01:47:47 +0800

    print in all caps finish

--- print_in_all_capital.py
+++ print_in_all_capital.py
@@ -2,4 +2,7 @@
 # this program will print the users full name in all capital letter
 
 # ask the user's full name
+fullname = input("Enter your full name: ")
+
 # convert the full name to uppercade and print it
+print(fullname.upper())    # print the full name in all capital letters
