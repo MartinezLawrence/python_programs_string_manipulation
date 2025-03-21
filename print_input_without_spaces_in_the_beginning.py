842dbe3c578959b9ced8c45bb7fc98c9eb4e86f1
unknown <lawrencekulet@gmail.com>
2025-03-22 01:30:49 +0800

    pseudocode prog1

---
+++ print_input_without_spaces_in_the_beginning.py
@@ -0,0 +1,6 @@
+# print input without spaces in the beginning
+# this program will print the inputted name without any spaces in the beginning
+
+# ask for the users full name with several spaces in the beginning
+# remove leading spaces and print the name
+# print the name without any leading spaces
