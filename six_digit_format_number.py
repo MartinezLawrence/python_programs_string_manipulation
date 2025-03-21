13e5789d96d5c4be484be6df221d0706f02f9881
unknown <lawrencekulet@gmail.com>
2025-03-22 01:43:08 +0800

    pseudocode

---
+++ six_digit_format_number.py
@@ -0,0 +1,5 @@
+# six digit format number
+# this program will print an inputted number from 1 - 1000 and print it in a six digit format with leading zeros beginning to complete the six digit format
+
+# prompt the user to input a number from 1 - 1000
+# print the number in a six digit format with leading zeros
