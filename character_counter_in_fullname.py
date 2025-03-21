90e8a7a6251005599725ab80500d1f260fb0e160
unknown <lawrencekulet@gmail.com>
2025-03-22 02:08:23 +0800

    pseudocode

---
+++ character_counter_in_fullname.py
@@ -0,0 +1,6 @@
+# character counter in fullname
+# this program will count how many characters the inoutted full name has
+
+# ask for the full name
+# count the characters in the full name
+# print the number of characters in the full name
